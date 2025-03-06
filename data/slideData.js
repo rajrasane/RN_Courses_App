f2ac1e5951ab6aaf61586519d9e30b24cada05a6
Raj Rasane <rasaneraj04@gmail.com>
2025-03-07 00:09:34 +0530

    seperated out data in files

---
+++ data/slideData.js
@@ -0,0 +1,22 @@
+const slides = [
+    {
+      id: 1,
+      image: require("../assets/images/slide1.png"),
+      title: "Free study materials for you",
+      description: "Access a variety of free resources to support your learning journey.",
+    },
+    {
+      id: 2,
+      image: require("../assets/images/slide2.png"),
+      title: "Learn coding at your own pace",
+      description: "Enroll in structured courses and enhance your coding skills step by step.",
+    },
+    {
+      id: 3,
+      image: require("../assets/images/slide3.png"),
+      title: "Create a personal study plan",
+      description: "Plan your learning effectively and stay on track with your goals.",
+    },
+  ];
+
+export default slides;
