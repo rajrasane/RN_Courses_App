921c007b42bf3e271f7a6c9605d9827f89178b27
Raj Rasane <rasaneraj04@gmail.com>
2025-03-11 22:57:01 +0530

    removed the unwated UI components

--- data/sidebarMenuItems.js
+++
@@ -1,9 +0,0 @@
-const menuItems = [
-    { title: 'Home', route: '/' },
-    { title: 'Profile', route: '/(tabs)/profile' },
-    {title : 'Notifications' , route:'/(screens)/notifications'},
-    { title: 'Settings', route: '/(screens)/settings' },
-    // { title: 'Logout', route: '/', isLogout: true }
-];
-
-export default menuItems;
